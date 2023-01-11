979ebd7e57481b83afb8e942e753ad5ce4102ddf
Anatoliy Sahakyan <anatoliysahakyan@Anatoliys-MacBook-Pro.local>
2023-01-11 21:52:48 +0400

    fixing record unlimited appending issue in the script

--- install_script.sh
+++ install_script.sh
@@ -10,7 +10,13 @@ cd nginx-trazler
 docker build -t nginx:$tag  . && cd ../ 
 sed -i'.original' -e "s/change/$tag/g" "restclient-helm-release/values.yaml"
 rm restclient-helm-release/*.original && helm install $release ./restclient-helm-release && sleep 10
-echo "127.0.0.1  trazler.info" >> /etc/hosts
+if grep -R "127.0.0.1  trazler.info" "/etc/hosts"
+then 
+  echo "Record exists!"
+else
+  echo "Adding record..."
+  echo "127.0.0.1  trazler.info" >> /etc/hosts
+fi
 minikube service $release-restclient-service &
 
 sed -i'.original' -e "s/$tag/change/g" "restclient-helm-release/values.yaml"
