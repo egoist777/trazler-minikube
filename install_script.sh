38083cfcf616e28bab963238586a7a412011f2d4
Anatoliy Sahakyan <anatoliysahakyan@Anatoliys-MacBook-Pro.local>
2023-01-11 22:08:30 +0400

    name fix

--- install_script.sh
+++ install_script.sh
@@ -8,8 +8,8 @@ read release
 eval $(minikube -p minikube docker-env)
 cd nginx-trazler
 docker build -t nginx:$tag  . && cd ../ 
-sed -i'.original' -e "s/change/$tag/g" "restclient-helm-release/values.yaml"
-rm restclient-helm-release/*.original && helm install $release ./restclient-helm-release && sleep 10
+sed -i'.original' -e "s/change/$tag/g" "trazler-helm-chart/values.yaml"
+rm trazler-helm-chart/*.original && helm install $release ./trazler-helm-chart && sleep 10
 if grep -R "127.0.0.1  trazler.info" "/etc/hosts"
 then 
   echo "Record exists!"
@@ -17,7 +17,7 @@ else
   echo "Adding record..."
   echo "127.0.0.1  trazler.info" >> /etc/hosts
 fi
-minikube service $release-restclient-service &
+minikube service $release-trazler-service &
 
-sed -i'.original' -e "s/$tag/change/g" "restclient-helm-release/values.yaml"
-rm restclient-helm-release/*.original
+sed -i'.original' -e "s/$tag/change/g" "trazler-helm-chart/values.yaml"
+rm trazler-helm-chart/*.original
